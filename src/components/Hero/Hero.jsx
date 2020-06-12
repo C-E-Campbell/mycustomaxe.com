90f8bcad2a9da7af07ee7261b4772d1032129716
Charles <charles.e.campbell1989@gmail.com>
2020-06-12 00:33:26 -0400

    finished desktop hero area

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,5 +2,18 @@ import React from 'react';
 import './Hero.scss';
 import { hero_img } from '../../assets/topImg.jpg';
 export default function Hero() {
-  return <div className="Hero"></div>;
+  return (
+    <div className="Hero">
+      <div className="Hero-Text">
+        <h2>
+          You built it<span>.</span>
+        </h2>
+        <h2>
+          Now show it<span>.</span>
+        </h2>
+
+        <a href="#">View Custom Builds Now</a>
+      </div>
+    </div>
+  );
 }
