a93a11863d6c174c9c03d6c22cbd0e9a083f4dd2
Charles <charles.e.campbell1989@gmail.com>
2020-06-12 00:14:41 -0400

    updates home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 import '../Header/Header.scss';
 export default function Header() {
-  return <div className="header">test</div>;
+  return (
+    <div className="header">
+      <div className="logo_container"></div>
+      <div className="link_container">
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import '../../assets/topImg.jpg';
 import Header from '../../components/Header/Header';
 import Hero from '../../components/Hero/Hero.jsx';
+import '../Home/Home.scss';
 export default function Home() {
   return (
     <Fragment>
